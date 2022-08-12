cc8e8b8790ca1c4c9a5f16c567adb1426f799438
giobitsa <giorgi.bitsadze01@gmail.com>
2022-08-12 14:12:06 +0400

    deploy

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import LoginComponent from '../components/LoginComponent/LoginComponent.jsx'
 import Navbar from '../components/Navbar/Navbar.jsx'
 
 const Login = () => {
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (localStorage.getItem('token') && localStorage.getItem('username')) {
+            navigate('/dashboard')
+        }
+    }, [])
+
+
     return (
         <>
             <Navbar />
