8a83b1983579f922fd3f6fb61acb75a64b777bdf
giobitsa <giorgi.bitsadze01@gmail.com>
2022-08-16 15:54:12 +0400

    added images

--- src/components/DashboardCharts/DashboardCharts.jsx
+++ src/components/DashboardCharts/DashboardCharts.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Grid } from "@mui/material";
+import Chart1 from "../../assets/chart1.jpg";
+import Chart2 from "../../assets/chart2.jpg";
+import Chart3 from "../../assets/chart3.jpg";
+import Chart4 from "../../assets/chart4.jpg";
 
 const DashboardCharts = () => {
-  return <div>DashboardCharts</div>;
+  return (
+    <Grid container>
+      <Grid item xs={12} md={6}>
+        <img src={Chart1} alt="chart" width="100%" />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <img src={Chart2} alt="chart" width="100%" />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <img src={Chart3} alt="chart" width="100%" />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <img src={Chart4} alt="chart" width="100%" />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default DashboardCharts;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Grid } from "@mui/material";
 import PageLayout from "../components/PageLayout/PageLayout";
-import RatingsComponent from "../components/RatingsComponent/RatingsComponent";
-import EngagementComponent from "../components/EngagementComponent/EngagementComponent";
-import ProgressChart from "../components/ProgressChart/ProgressChart";
-import BubbleChart from "../components/BubbleChart/BubbleChart";
+// import { Grid } from "@mui/material";
+// import RatingsComponent from "../components/RatingsComponent/RatingsComponent";
+// import EngagementComponent from "../components/EngagementComponent/EngagementComponent";
+// import ProgressChart from "../components/ProgressChart/ProgressChart";
+// import BubbleChart from "../components/BubbleChart/BubbleChart";
+import DashboardCharts from "../components/DashboardCharts/DashboardCharts";
 
 const Dashboard = () => {
-  return <PageLayout>
-    <Grid container spacing={5}>
+  return (
+    <PageLayout>
+      {/* <Grid container spacing={5}>
       <Grid item xs={12} md={6}>
         <RatingsComponent />
       </Grid>
@@ -21,8 +23,10 @@ const Dashboard = () => {
       <Grid item xs={12} md={6}>
         <ProgressChart />
       </Grid>
-    </Grid>
-  </PageLayout>;
+    </Grid> */}
+      <DashboardCharts />
+    </PageLayout>
+  );
 };
 
 export default Dashboard;
